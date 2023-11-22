69e8d2587f84eb0724af763a7d6d49d061294a54
Khawla BADDAR <80429328+khawla-94@users.noreply.github.com>
2023-11-22 23:46:49 +0100

    Cat_Dog_Classifier_App.py

--- Cat_Dog_Classifier_App.py
+++ Cat_Dog_Classifier_App.py
@@ -1,67 +1,70 @@
 import streamlit as st
-import tensorflow as tf
+import requests
 from PIL import Image
 import numpy as np
-import requests
+import tensorflow as tf
 from io import BytesIO
 import tempfile
 
+# Fonction pour charger le modèle depuis GitHub
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model_url = "https://github.com/khawla-94/cat_dog_classifier/raw/main/cat_dog_classifier_model.tflite"
-  model_content = requests.get(model_url).content
-  # Save the file in a temporary file:
-  with tempfile.NamedTemporaryFile(delete = False) as temp_model_file:
-    temp_model_file.write(model_content)
-  print(f"Temporary model file path: {temp_model_file.name}")
-  
-  # Initialize the interpreter with a temporary file:
-  model = tf.lite.Interpreter(model_path = temp_model_file.name)
-  model.allocate_tensors()
-  return model
+    model_url = "https://github.com/khawla-94/cat_dog_classifier/raw/main/cat_dog_classifier_model.tflite"
+    model_content = requests.get(model_url).content
 
+    # Enregistrer le contenu dans un fichier temporaire
+    with tempfile.NamedTemporaryFile(delete=False) as temp_model_file:
+        temp_model_file.write(model_content)
+
+    # Initialiser l'interpréteur avec le fichier temporaire
+    model = tf.lite.Interpreter(model_path=temp_model_file.name)
+    model.allocate_tensors()
+    return model
+
+# Fonction pour faire une prédiction
+def predict_image(img, model):
+  
+    img = img.resize((200, 200))
+
+    img_array =  np.array(img).astype('float32')
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0
+
+    # Préparer les données pour le modèle
+    input_tensor_index = model.get_input_details()[0]['index']
+    output = model.tensor(model.get_output_details()[0]['index'])
+
+    # Faire une prédiction
+    model.set_tensor(input_tensor_index, img_array)
+    model.invoke()
+    prediction = output()
+
+    if prediction[0, 0] > 0.5:
+        return 'Dog'
+    else:
+        return 'Cat'
+
+# Chargement du modèle
+model = load_model()
+
+# Interface utilisateur Streamlit
 st.write("""
 # MSDE5 : Deep Learning Project
 ## Cat Vs Dog Classification using CNN and Transfer Learning
 """)
-
 st.write("Upload an image to classify whether it's a cat or a dog.")
 
-# File uploader
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
-# make predictions:
-def predict_image(img, model):
-  img = Image.open(uploaded_file).convert('RGB')
-  img = img.resize((200, 200))
-  img_array = tf.keras.preprocessing.image.img_to_array(img)
-  img_array = tf.expand_dims(img_array, 0)
-  img_array /= 255.0
-
-  input_tensor_index = model.get_input_details()[0]['index']
-  output = model.tensor(model.get_output_details()[0]['index'])
-
-  model.set_tensor(input_tensor_index, img_array)
-  model.invoke()
-  prediction = output()
-  return prediction
-  
-# Loading the model:
-model = load_model()
+uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-  image = Image.open(uploaded_file)
-  # Display the uploaded image and the prediction result
-  st.image(image, caption="Uploaded Image", use_column_width=True)
-  # Predict:
-  prediction = predict_image(image, model)
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
-if prediction[0] > 0.5:
-  st.success(f"Prediction: Dog (Probability: {prediction[0]:.2f})")
-else:
-  st.success(f"Prediction: Cat (Probability: {1 - prediction[0]:.2f})")
+    # Faire une prédiction avec le modèle
+    result = predict_image(image, model)
     
-if prediction == 'Dog':
-  st.success("Prediction: Dog")
-else:
-  st.success("Prediction: Cat")
+    # Afficher la prédiction en mettant en évidence le résultat
+    if result == 'Dog':
+        st.success("Prediction: Dog 🐶")
+    else:
+        st.success("Prediction: Cat 🐱")
