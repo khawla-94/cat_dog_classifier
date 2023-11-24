8d9178b473f1a997fb6e26aeafcb5277275ad87b
Khawla BADDAR <80429328+khawla-94@users.noreply.github.com>
2023-11-24 16:12:46 +0100

    Cat_Dog_Classifier_App.py

--- Cat_Dog_Classifier_App.py
+++ Cat_Dog_Classifier_App.py
@@ -55,8 +55,7 @@ st.write("""
 
 st.sidebar.image("https://miro.medium.com/v2/resize:fit:1400/format:webp/1*EvMbMNRHm_aOf1n4tDO1Xg.jpeg", width=250)
 st.sidebar.write("This is a classification model of cat and dog images")
-st.sidebar.text("This project was made by : KHAWLA BADDAR and AYMANE AL AZHARI")
-st.sidebar.text("KHAWLA BADDAR and AYMANE AL AZHARI")
+st.markdown("This project was made by : **KHAWLA BADDAR** and **AYMANE AL AZHARI**")
 st.write("Upload an image to classify whether it's a cat or a dog.")
 
 uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
@@ -73,81 +72,3 @@ if uploaded_file is not None:
         st.success("Prediction: It's a Dog ")
     else:
         st.success(f"Prediction: It's a Cat ")
-
-# # Import Dependencies:
-# import streamlit as st
-# import tensorflow as tf
-# from PIL import Image
-# import numpy as np
-# import requests
-# from io import BytesIO
-
-# # Loading the model:
-# @st.cache(allow_output_mutation = True)
-# def load_model():
-#     model_url = "https://github.com/khawla-94/cat_dog_classifier/raw/main/cat_dog_classifier_model.tflite" 
-#     model_content = requests.get(model_url).content
-#     model = tf.lite.Interpreter(model_content = model_content)
-#     model.allocate_tensors()
-#     return model
-
-# # Get input and output details:
-# interpreter = load_model()
-# input_details = interpreter.get_input_details()
-# output_details = interpreter.get_output_details()
-
-# # Define the image size expected by the model:
-# input_shape = input_details[0]['shape'][1:3]
-
-# # PreProcess the model:
-
-# def preprocess_image(image_path):
-#     try:
-#         img = Image.open(image_path).convert('RGB')
-#         img = img.resize(input_shape, Image.BICUBIC)
-#         img_array = np.array(img, dtype = np.float32) / 255.0
-#         img_array = np.expand_dims(img_array, axis = 0)
-#         return img_array
-#     except Exception as e:
-#         st.error(f"Error preprocessing image: {e}")
-#         return None
-
-# # Function to make predictions:
-
-# def predict_image(image_path):
-#     input_data = preprocess_image(image_path)
-#     if input_data is None:
-#         return None
-#     try:
-#         print("Input data shape:", input_data.shape if hasattr(input_data, 'shape') else "Not available")
-#         interpreter.set_tensor(input_details[0]['index'], input_data)
-#         interpreter.invoke()
-#         output_data = interpreter.get_tensor(output_details[0]['index'])
-#         return output_data[0][0]
-#     except Exception as e:
-#         st.error(f"Error setting tensor: {e}")
-#         return None
-
-# # Streamlit App:
-# st.write("""
-# # MSDE5 : Deep Learning Project
-# ## Cat Vs Dog Classification using CNN and Transfer Learning
-# """)
-# st.sidebar.image("https://miro.medium.com/v2/resize:fit:1400/format:webp/1*EvMbMNRHm_aOf1n4tDO1Xg.jpeg", width=250)
-
-# st.write("Upload an image to classify whether it's a cat or a dog.")
-
-# uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
-
-# # Display the uploaded file:
-
-# if uploaded_file is not None:
-#     image = Image.open(uploaded_file)
-#     st.image(image, caption = "Uploaded Image", use_column_width = True)
-#     # make predictions:
-#     prediction = predict_image(uploaded_file)
-#     # Display the prediction:
-#     if prediction > 0.5:
-#         st.success("Prediction: It's a Dog ")
-#     else:
-#         st.success("Prediction: It's a Cat")
