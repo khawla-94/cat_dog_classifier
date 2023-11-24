e7d0de5098f970cfb2fae798e6475850d985786a
Khawla BADDAR <80429328+khawla-94@users.noreply.github.com>
2023-11-24 15:25:04 +0100

    Cat_Dog_Classifier_App.py

--- Cat_Dog_Classifier_App.py
+++ Cat_Dog_Classifier_App.py
@@ -1,70 +1,132 @@
+# Import Dependencies:
 import streamlit as st
-import requests
+import tensorflow as tf
 from PIL import Image
 import numpy as np
-import tensorflow as tf
-from io import BytesIO
-import tempfile
 
-# Fonction pour charger le modèle depuis GitHub
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model_url = "https://github.com/khawla-94/cat_dog_classifier/raw/main/cat_dog_classifier_model.tflite"
-    model_content = requests.get(model_url).content
+# Loading the model:
+@st.cache(allow_output_mutation = True)
+model_url = "https://github.com/khawla-94/cat_dog_classifier/raw/main/cat_dog_classifier_model.tflite" 
+interpreter = tf.lite.Interpreter(model_path = model_url)
+interpreter.alloacate_tensors()
 
-    # Enregistrer le contenu dans un fichier temporaire
-    with tempfile.NamedTemporaryFile(delete=False) as temp_model_file:
-        temp_model_file.write(model_content)
+# Get input and output details:
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-    # Initialiser l'interpréteur avec le fichier temporaire
-    model = tf.lite.Interpreter(model_path=temp_model_file.name)
-    model.allocate_tensors()
-    return model
+# Define the image size expected by the model:
+input_shape = input_details[0]['shape'][1:3]
 
-# Fonction pour faire une prédiction
-def predict_image(img, model):
-  
-    img = Image.open(uploaded_file).convert('RGB')
-    img = img.resize((200, 200))
-    img_array =  tf.keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, axis=0)
-    img_array /= 255.0
+# PreProcess the model:
 
-    # Préparer les données pour le modèle
-    input_tensor_index = model.get_input_details()[0]['index']
-    output = model.tensor(model.get_output_details()[0]['index'])
+def preprocess_image(image_path):
+    img = Image.open(image_path).convert('RGB')
+    img = img.resize(input_shape, Image.ANTIALIAS)
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis = 0)
+    return img_array
 
-    # Faire une prédiction
-    model.set_tensor(input_tensor_index, img_array)
-    model.invoke()
-    prediction = output()
+# Function to make predictions:
 
-    if prediction[0, 0] > 0.5:
-        return 'Dog'
-    else:
-        return 'Cat'
+def predict_image(image_path):
+    input_data = preprocess_image(image_path)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    return output_data[0][0]
 
-# Chargement du modèle
-model = load_model()
-
-# Interface utilisateur Streamlit
+# Streamlit App:
 st.write("""
 # MSDE5 : Deep Learning Project
 ## Cat Vs Dog Classification using CNN and Transfer Learning
 """)
+st.sidebar.image("https://miro.medium.com/v2/resize:fit:1400/format:webp/1*EvMbMNRHm_aOf1n4tDO1Xg.jpeg", width=250)
+
 st.write("Upload an image to classify whether it's a cat or a dog.")
 
 uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
 
+# Display the uploaded file:
+
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption = "Uploaded Image", use_column_width = True)
+    # make predictions:
+    prediction = predict_image(uploaded_file)
+    # Display the prediction:
+    if prediction >= 0.5:
+        st.success(f"Prediction: Dog (Probability: {prediction:.2f})")
+    else:
+        st.success(f"Prediction: Cat (Probability: {1 - prediction:.2f})")
 
-    # Faire une prédiction avec le modèle
-    result = predict_image(image, model)
+
+# import streamlit as st
+# import requests
+# from PIL import Image
+# import numpy as np
+# import tensorflow as tf
+# from io import BytesIO
+# import tempfile
+
+# # Fonction pour charger le modèle depuis GitHub
+# @st.cache(allow_output_mutation=True)
+# def load_model():
+#     model_url = "https://github.com/khawla-94/cat_dog_classifier/raw/main/cat_dog_classifier_model.tflite"
+#     model_content = requests.get(model_url).content
+
+#     # Enregistrer le contenu dans un fichier temporaire
+#     with tempfile.NamedTemporaryFile(delete=False) as temp_model_file:
+#         temp_model_file.write(model_content)
+
+#     # Initialiser l'interpréteur avec le fichier temporaire
+#     model = tf.lite.Interpreter(model_path=temp_model_file.name)
+#     model.allocate_tensors()
+#     return model
+
+# # Fonction pour faire une prédiction
+# def predict_image(img, model):
+  
+#     img = Image.open(uploaded_file).convert('RGB')
+#     img = img.resize((200, 200))
+#     img_array =  tf.keras.preprocessing.image.img_to_array(img)
+#     img_array = tf.expand_dims(img_array, axis=0)
+#     img_array /= 255.0
+
+#     # Préparer les données pour le modèle
+#     input_tensor_index = model.get_input_details()[0]['index']
+#     output = model.tensor(model.get_output_details()[0]['index'])
+
+#     # Faire une prédiction
+#     model.set_tensor(input_tensor_index, img_array)
+#     model.invoke()
+#     prediction = output()
+
+#     if prediction[0, 0] > 0.5:
+#         return 'Dog'
+#     else:
+#         return 'Cat'
+
+# # Chargement du modèle
+# model = load_model()
+
+# # Interface utilisateur Streamlit
+# st.write("""
+# # MSDE5 : Deep Learning Project
+# ## Cat Vs Dog Classification using CNN and Transfer Learning
+# """)
+# st.write("Upload an image to classify whether it's a cat or a dog.")
+
+# uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
+
+# if uploaded_file is not None:
+#     image = Image.open(uploaded_file)
+#     st.image(image, caption="Uploaded Image", use_column_width=True)
+
+#     # Faire une prédiction avec le modèle
+#     result = predict_image(image, model)
     
     # Afficher la prédiction en mettant en évidence le résultat
-    if result == 'Dog':
-        st.success("Prediction: It's a Dog ")
-    else:
-        st.success(f"Prediction: It's a Cat ")
+    # if result == 'Dog':
+    #     st.success("Prediction: It's a Dog ")
+    # else:
+    #     st.success(f"Prediction: It's a Cat ")
